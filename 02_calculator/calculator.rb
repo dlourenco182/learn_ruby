18d69f86fcc00e26dd3a006bb31bc6ee5e86e016
David Luciano Gonçalves Lourenço <david.lourenco11@gmail.com>
2018-06-03 16:58:07 +0100

    Still trying

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -8,3 +8,29 @@ end
 def sum(array)
     array.inject(0){|sum,x| sum + x }
 end
+def multiply(arr)
+    arr.inject(1) do |total, current|
+      total * current
+    end
+  end
+
+def power num1, num2
+	total = 1
+	factor = num2
+	while factor >0 do
+		total *= num1
+		factor -=1
+	end
+	total
+end
+
+def factorial num1
+	total = 1
+	i = num1
+	while i > 0 do
+		total *= i
+		i -= 1
+	end
+	total
+
+end
